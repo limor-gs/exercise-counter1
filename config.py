d069044915deb60a4630fd945b10f663e49cdea2
limor-gs <limor@gigaspaces.com>
2018-09-01 13:06:20 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,9 @@ import os
 import uuid
 
 
-APP_ID_PATH = '/tmp/app/app_id'
+with open('/etc/hostname', 'r') as myfile:
+	data = myfile.read()
+APP_ID_PATH = f"/tmp/app/app_id_{data}"
 if os.path.exists(APP_ID_PATH):
 	with open(APP_ID_PATH) as app_id_file:
 		app_id = app_id_file.read()
