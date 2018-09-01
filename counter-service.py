04457c487b8008281e9d7c441b1279b2b890ee88
limor-gs <limor@gigaspaces.com>
2018-09-01 21:18:30 +0300

    Update counter-service.py

--- counter-service.py
+++ counter-service.py
@@ -1,3 +1,36 @@
 
-print "hello world"
+from redis import StrictRedis
+from flask import Flask, request
 
+app = Flask(__name__)
+app.config.from_object('config.AppConfig')
+
+
+class Counter():
+	def __init__(self):
+		self.app_id = app.config.get('APP_ID')	
+		connection = {
+			'host': app.config.get('REDIS_HOST'),
+			'password': app.config.get('REDIS_PASS'),
+			'port': app.config.get('REDIS_PORT'),
+			'db': 0
+		}
+		self.redis = StrictRedis(**connection)		
+
+	def increase(self):				
+		return self.redis.incr(self.app_id)
+
+	def value(self):				
+		return self.redis.get(self.app_id) or '0'
+
+
+@app.route('/', methods=['GET', 'POST'])
+def hello():
+	if request.method == 'POST':
+		response = f"Increasing counter. new value: {Counter().increase()} "		
+	else:
+		response = f"Counter value: {int(Counter().value())}"
+	return response
+
+if __name__ == '__main__':    	
+	app.run(host='0.0.0.0', port=5000)
